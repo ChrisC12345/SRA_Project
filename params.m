cc48a86d886f0ca460f11916974f40126ccaa13e
lando <lando@169-231-136-252.wireless.ucsb.edu>
2026-07-08 15:25:44 -0700

    Dryden finished

---
+++ params.m
@@ -0,0 +1,10 @@
+function parameters = params()
+    params.m = 1.0;
+    params.g = 9.81;
+    params.wind_speed = 1.0;
+    params.air_density = 1.225;
+    params.arm_length = 0.225;
+    params.thrust_coef = 3.0e-5;
+    params.torque_coef = 7.5e-7;
+    parameters = params;
+end
