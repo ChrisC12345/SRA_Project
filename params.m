8775fe1a6e888bf3ec93b97d90b5446d0e88809a
ChrisC12345 <chrisjiaming2020@gmail.com>
2026-07-10 11:19:26 -0700

    added wind mean speed

--- params.m
+++ params.m
@@ -3,15 +3,25 @@ function parameters = params()
     k_f = 3.0e-5;
     k_m = 7.5e-7;
     l = 0.225;
+    wind_speed = 20;
 
     params.m = 1.0;
     params.g = 9.81;
-    params.wind_speed = 1.0;
+
+    params.wind_speed = wind_speed;
+    params.V_mean = [wind_speed;0;0];
     params.air_density = 1.225;
+
     params.arm_length = l;
+    params.inertia = [0.01,0,0;0,0.01,0;0,0,0.02];
+
     params.thrust_coef = k_f;
     params.torque_coef = k_m;
-    params.inertia = [0.01,0,0;0,0.01,0;0,0,0.02];
+    params.Cd_fuse = 1.0;
+    params.Ax = 0.005;
+    params.Ay = 0.005;
+    params.Az = 0.03;
+    params.kh = 1.0e-4;
 
     A = [ k_f,k_f,k_f,k_f;
         0,-k_f*l,0,k_f*l;
