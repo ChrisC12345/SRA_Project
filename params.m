bbdb56abc2aa39227a53257aa4436e15a50330ee
ChrisC12345 <chrisjiaming2020@gmail.com>
2026-07-12 14:44:43 -0700

    motor spin up done

--- params.m
+++ params.m
@@ -3,10 +3,12 @@ function parameters = params()
     k_f = 3.0e-5;
     k_m = 7.5e-7;
     l = 0.225;
-    wind_speed = 20;
+    wind_speed = 10;
+    m = 1.0;
+    g = 9.81;
 
-    p.m = 1.0;
-    p.g = 9.81;
+    p.m = m;
+    p.g = g;
 
     p.wind_speed = wind_speed;
     p.V_mean = [wind_speed;0;0];
@@ -15,6 +17,7 @@ function parameters = params()
     p.arm_length = l;
     p.inertia = [0.01,0,0;0,0.01,0;0,0,0.02];
 
+    p.tau_motor = 0.05;
     p.thrust_coef = k_f;
     p.torque_coef = k_m;
     p.Cd_fuse = 1.0;
@@ -27,6 +30,8 @@ function parameters = params()
         0,-k_f*l,0,k_f*l;
         -k_f*l,0,k_f*l,0;
         -k_m,k_m,-k_m,k_m];
+
+    p.w_hover   = sqrt((m*g/4)/k_f)*ones(4,1); % hover motor speed [rad/s]
 
     p.mixer_inv = inv(A);   % precompute the inverse
 
