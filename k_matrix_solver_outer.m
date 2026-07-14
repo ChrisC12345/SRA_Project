8d527ca97bd922ebfb4326f63e756736968f6ab2
ChrisC12345 <chrisjiaming2020@gmail.com>
2026-07-13 22:05:37 -0700

    more tuning

--- k_matrix_solver_outer.m
+++ k_matrix_solver_outer.m
@@ -6,8 +6,8 @@ function K_outer = k_matrix_solver_outer()
     0,0,0,0,0,0
     0,0,0,0,0,0
     0,0,0,0,0,0
-    
     ];
+
     B_outer = [
     0,0,0
     0,0,0
@@ -16,8 +16,11 @@ function K_outer = k_matrix_solver_outer()
     0,1,0
     0,0,1
     ];
-    Q_outer = diag([2,2,4,1,1,2]);
+
+    Q_outer = diag([4,4,8,1,1,2]);
     R_outer = diag([0.1,0.1,0.1]);
     
     K_outer = lqr(A_outer,B_outer,Q_outer,R_outer);
+
+    disp(eig(A_outer - B_outer * K_outer));
 end
