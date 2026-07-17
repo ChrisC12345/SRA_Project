91aa6e2d5d2919b5f3d6529c477ee70dff5404e8
ChrisC12345 <chrisjiaming2020@gmail.com>
2026-07-16 20:45:26 -0700

    tuning

--- k_matrix_solver_inner.m
+++ k_matrix_solver_inner.m
@@ -22,10 +22,10 @@ function K_inner = k_matrix_solver_inner()
     0,0,1/I_zz
     ];
     
-    Q_inner = diag([40 40 12, 1 1 0.4]);   % angles ~10x, rates ~2x
+    Q_inner = diag([80 80 12, 1 1 0.4]);   % angles ~10x, rates ~2x
     R_inner = diag([16 16 100]);             % unchanged
     
     K_inner = lqr(A_inner, B_inner, Q_inner, R_inner);
 
-    %damp(eig(A_inner - B_inner * K_inner))
+    damp(eig(A_inner - B_inner * K_inner))
 end
